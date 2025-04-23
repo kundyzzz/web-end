9275508e542d3e1dcc8b5a9a5185457485b1bcbe
kundyzzz <159567667+kundyzzz@users.noreply.github.com>
2025-04-24 02:36:33 +0500

    Update urls.py

--- quizlet/flashcards/urls.py
+++ quizlet/flashcards/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import get_user_decks, create_flashcard, DeckListCreateAPIView, DeckDetailAPIView
 
 urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('api/decks/user/', get_user_decks, name='get_user_decks'),
+    path('api/flashcards/create/', create_flashcard, name='create_flashcard'),
+
+    path('api/decks/', DeckListCreateAPIView.as_view(), name='deck_list_create'),
+    path('api/decks/<int:pk>/', DeckDetailAPIView.as_view(), name='deck_detail'),
 ]
